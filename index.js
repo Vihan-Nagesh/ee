e1ac2008614c586482784514979794285e9cf74e
Vihan-Nagesh <155616383+Vihan-Nagesh@users.noreply.github.com>
2025-08-22 20:34:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve everything in this folder (including GIFs) as static files
 app.use(express.static(__dirname));
