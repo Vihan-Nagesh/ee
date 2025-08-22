a9ac95ca6d936c2f51dad1005f9bb39edbfe833f
Vihan-Nagesh <155616383+Vihan-Nagesh@users.noreply.github.com>
2025-08-22 20:06:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// always serve this exact GIF at /gif
-app.get("/gif", (req, res) => {
-  res.sendFile(path.join(__dirname, "Furinacaranimeanimefypanimereelsanimeeditanimememesfypfypgitaagotohgensh-ezgif.com-rotate.gif"));
-});
+// Serve everything in this folder (including GIFs) as static files
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
-  console.log(`GIF available at: http://localhost:${port}/gif`);
+  console.log(`Server running at: http://localhost:${port}`);
 });
