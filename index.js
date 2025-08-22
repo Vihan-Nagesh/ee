e7d1e74809776a06b5433cf02d33d68e7e0067b7
Vihan-Nagesh <155616383+Vihan-Nagesh@users.noreply.github.com>
2025-08-22 21:09:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get("/hello", (req, res) => {
   res.send("🚂 Railway server is running!");
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
